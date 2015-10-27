efe20ed9ada0066ec9a99dd59717f2825abb1bc1
bbouley <b.bouley@hotmail.co.uk>
2015-10-27 16:01:08 -0600

    added updated schema lecture

---
+++ server/models/users.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var User = new Schema ({
+    name : String,
+    events : [{type : Schema.Types.ObjectId, ref : 'events'}]
+});
+
+module.exports = mongoose.model('users', User);
